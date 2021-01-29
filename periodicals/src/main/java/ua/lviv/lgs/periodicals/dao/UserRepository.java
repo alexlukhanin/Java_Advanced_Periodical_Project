f68ac4f435ce02382b83381bf7ff5138d926eb31
Oleksandr Lukhanin <alex.lukhanin@hotmail.com>
2021-01-29 02:35:56 +0200

    Commit 04 - add dao layer using Spring data

---
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package ua.lviv.lgs.periodicals.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.lviv.lgs.periodicals.domain.User;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByEmail(String email);
+}
