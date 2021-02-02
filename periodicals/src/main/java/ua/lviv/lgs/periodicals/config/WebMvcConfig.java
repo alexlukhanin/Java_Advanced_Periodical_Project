86ef96b5340ab034a7559d4ee3dba4304e0bc189
Oleksandr Lukhanin <alex.lukhanin@hotmail.com>
2021-02-02 10:32:40 +0200

    add UserController

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/config/WebMvcConfig.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/config/WebMvcConfig.java
@@ -16,8 +16,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("login").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
         registry.addViewController("/403").setViewName("403");
+       // registry.addViewController("registration").setViewName("registration");
     }
 
     @Bean
